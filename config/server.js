2e16d6e4c0af97ab3d2e065ea121c25ef80a3e1e
mpalinka <m.palinka@hypermedia.pl>
2024-03-28 14:36:09 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -7,4 +7,8 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  // Ustaw tryb na development, jeśli NODE_ENV jest ustawione na 'development'
+  ...(process.env.NODE_ENV === 'development' && { 
+    devMode: true 
+  }),
 });
